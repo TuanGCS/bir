771888f6ec24bab1b003b09cc50df63ff8039cca
nadeshr <nadeshramanathan88@gmail.com>
2014-03-20 10:54:16 +0000

    Half Working Router - Fails LPM and ARP tests - Need to further pipeline the LPM module - SmartExplorer was needed to generate this bitfile - Critical path was input arbiter for some reason

--- HW_stub/NetFPGA-10G-live/projects/bir_starter/test/both_invalidttl_packet/run.py
+++ HW_stub/NetFPGA-10G-live/projects/bir_starter/test/both_invalidttl_packet/run.py
@@ -51,10 +51,10 @@ for i in range(30):
                                dst_IP=DST_IP, src_IP=SRC_IP,
                                pkt_len=random.randint(60,1514))
             sent_pkt.ttl = TTL
-            #nftest_send_phy('nf%d'%port, sent_pkt)
-            #nftest_expect_dma('nf%d'%port, sent_pkt)
-            nftest_send_phy('nf1', sent_pkt)
-            nftest_expect_dma('nf1', sent_pkt)
+            nftest_send_phy('nf%d'%port, sent_pkt)
+            nftest_expect_dma('nf%d'%port, sent_pkt)
+            #nftest_send_phy('nf1', sent_pkt)
+            #nftest_expect_dma('nf1', sent_pkt)
     else:
 	DA = routerMAC[0]
 	sent_pkt = make_IP_pkt(dst_MAC=DA, src_MAC=SA,
