d9637d0e4ba1fc5b33cf95e95e2abf17b0fae7eb
mininet <mininet@mininet-vm.(none)>
2014-04-03 16:11:30 +0100

    Check whether DNS is not QR

--- SW_stub/dns.c
+++ SW_stub/dns.c
@@ -54,14 +54,20 @@ void dns_onreceive(packet_info_t* pi, packet_udp_t * udp, packet_dns_t * dns) {
 	const uint16_t totalquestions = ntohs(dns->totalquestions);
 	printf("Total questions %d\n", totalquestions);
 
-	dns_query_ho_t * questions = dns_mallocandparse_query_array((byte *) ((byte *) dns + sizeof(packet_dns_t)), totalquestions);
+	if (!dns->QR) {
 
-	int i;
-	for (i = 0; i < totalquestions; i++) {
-		int j;
-		for (j = 0; j < questions[i].count; j++)
-			printf("Qname %d: %s; ", j, questions[i].query_names[j]);
-		printf("type %d, class %d\n", questions[i].qtype, questions[i].qclass);
+		dns_query_ho_t * questions = dns_mallocandparse_query_array((byte *) ((byte *) dns + sizeof(packet_dns_t)), totalquestions);
+
+		int i;
+		for (i = 0; i < totalquestions; i++) {
+			int j;
+			for (j = 0; j < questions[i].count; j++)
+				printf("Qname %d: %s; ", j, questions[i].query_names[j]);
+			printf("type %d, class %d\n", questions[i].qtype, questions[i].qclass);
+		}
+
+	} else {
+		// TODO
 	}
 
 	dns_free_query_array(questions, totalquestions);
