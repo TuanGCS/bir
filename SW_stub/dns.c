c829198c5fb36d9a31d3c2281d561a2c9fd6f4d3
mininet <mininet@mininet-vm.(none)>
2014-04-04 13:34:32 +0100

    Fixed compilation

--- SW_stub/dns.c
+++ SW_stub/dns.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #include <stdint.h>
+#include <assert.h>
 #include "packets.h"
 #include "sr_router.h"
 #include "dns.h"
@@ -46,7 +47,7 @@ dns_query_ho_t * dns_mallocandparse_other_array(byte * address, int size) {
 	for (id = 0; id < size; id++) {
 		dns_cache_entry_t * curr = &array[id];
 		curr->count = 0;
-		curr->query_names = NULL;
+		curr->names = NULL;
 
 		uint8_t char_size;
 		while ((char_size = *(address++))!=0) {
@@ -56,12 +57,12 @@ dns_query_ho_t * dns_mallocandparse_other_array(byte * address, int size) {
 			memcpy(string, address, char_size);
 			curr->count++;
 
-			if (curr->query_names == NULL)
-				curr->query_names = (char **) malloc(sizeof(char *)*curr->count);
+			if (curr->names == NULL)
+				curr->names = (char **) malloc(sizeof(char *)*curr->count);
 			else
-				curr->query_names = (char **) realloc((void *) curr->query_names , sizeof(char *)*curr->count);
+				curr->names = (char **) realloc((void *) curr->names , sizeof(char *)*curr->count);
 
-			curr->query_names[curr->count-1] = string;
+			curr->names[curr->count-1] = string;
 
 			address+=char_size;
 		}
@@ -70,7 +71,7 @@ dns_query_ho_t * dns_mallocandparse_other_array(byte * address, int size) {
 		curr->class = ntohs(*( (uint16_t *)  address ) ); address += 2;
 		curr->ttl = ntohs(*( (uint16_t *) address ) ); address += 4;
 		curr->length = ntohs(*( (uint16_t *) address ) ); address += 2;
-		curr->addr_ip = ntohs(*( (uint16_t *) address ) ); address += 4;
+		//curr->addr_ip = ntohs(*( (uint16_t *) address ) ); address += 4;
 	}
 	return array;
 }
@@ -84,6 +85,75 @@ void dns_free_query_array(dns_query_ho_t * questions, int size) {
 		if (questions[i].query_names != NULL) free (questions[i].query_names);
 	}
 	free (questions);
+}
+
+dns_answer_proto_packet_t * dns_malloc_answer(void) {
+	dns_answer_proto_packet_t * answer = (dns_answer_proto_packet_t *) malloc(sizeof(dns_answer_proto_packet_t));
+	answer->totalsize = sizeof(packet_dns_t);
+	answer->packet = malloc(answer->totalsize);
+	packet_dns_t * dns = (packet_dns_t *) answer->packet;
+
+
+	// TODO! populate!
+	dns->totaladditionalrrs = 0;
+	dns->totalanswerrrs = 0;
+	dns->totalauthorityrrs = 0;
+	dns->totalquestions = 0;
+
+	return answer;
+}
+
+int calctotalsizeofencoding(char ** name, int name_count) {
+	int total = 0;
+	int i;
+	for (i = 0; i < name_count; i++)
+		total += strlen(name[i])+1;
+	return total + 1;
+}
+
+void dns_answer_add_A_answer_to_end(dns_answer_proto_packet_t * answer, char ** name, int name_count, addr_ip_t ip) {
+	typedef struct dns_resource_middle_entry {
+		uint16_t type;
+		uint16_t class;
+		uint32_t ttl;
+		uint16_t length;
+	} dns_resource_middle_entry_t;
+
+
+	const int totalsize = calctotalsizeofencoding(name, name_count) + sizeof(dns_cache_entry_t) + sizeof(addr_ip_t);
+	byte * answerstartpos = answer->packet + answer->totalsize;
+
+	answer->totalsize += totalsize;
+	answer->packet = realloc(answer->packet, answer->totalsize);
+	packet_dns_t * dns = (packet_dns_t *) answer->packet;
+	dns->totalanswerrrs++;
+
+	int i;
+	for (i = 0; i < name_count; i++) {
+		const int len_name = strlen(name[i]);
+		*(answerstartpos++) = len_name;
+		memcpy(answerstartpos, name, len_name);
+		answerstartpos+=len_name;
+	}
+	*(answerstartpos++) = 0;
+
+	dns_resource_middle_entry_t * middle_entry = (dns_resource_middle_entry_t *) answerstartpos;
+	answerstartpos+=sizeof(answerstartpos);
+
+	//TODO!
+	//middle_entry->class = htons();
+	middle_entry->length = htons(sizeof(addr_ip_t));
+	//middle_entry->ttl = htonl();
+	middle_entry->type = htons(DNS_TYPE_A);
+
+	*((addr_ip_t *) (answerstartpos++)) = ip;
+
+	assert(answerstartpos == answer->packet + answer->totalsize);
+}
+
+void dns_free_answer(dns_answer_proto_packet_t * answer) {
+	free (answer->packet);
+	free (answer);
 }
 
 void dns_onreceive(packet_info_t* pi, packet_udp_t * udp, packet_dns_t * dns) {
--- SW_stub/dns.h
+++ SW_stub/dns.h
@@ -2,12 +2,16 @@
 #ifndef DNS_H_
 #define DNS_H_
 
+#define DNS_TYPE_A (0)
+#define DNS_TYPE_NS (1)
+
 typedef struct dns_query_ho {
 	char ** query_names;
 	int count;
 	uint16_t qtype;
 	uint16_t qclass;
 } dns_query_ho_t;
+
 
 typedef struct dns_cache_entry {
 	char ** names;
@@ -26,6 +30,12 @@ typedef struct dns_db_entry {
 	uint16_t length;
 	byte * rdata;
 } dns_db_entry_t;
+
+typedef struct dns_r_proto_packet {
+	byte * packet;
+	int totalsize;
+} dns_answer_proto_packet_t;
+
 
 void dns_onreceive(packet_info_t* pi, packet_udp_t * udp, packet_dns_t * dns);
 
