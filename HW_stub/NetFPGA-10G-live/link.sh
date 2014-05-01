81a191b1a4e7060185c92a7e81802bfeabd563e4
nadeshr <nadeshramanathan88@gmail.com>
2014-05-01 18:25:29 +0100

    script added

--- HW_stub/NetFPGA-10G-live/link.sh
+++ HW_stub/NetFPGA-10G-live/link.sh
@@ -1,2 +1,3 @@
 ln -s  $(pwd)/fpga_nadesh_prog_script.sh /usr/bin/progfpga_nadesh
 ln -s  $(pwd)/fpga_ref_prog_script.sh /usr/bin/progfpga_ref 
+ln -s  $(pwd)/fpga_nadesh_prog_script2.sh /usr/bin/progfpga2_nadesh
