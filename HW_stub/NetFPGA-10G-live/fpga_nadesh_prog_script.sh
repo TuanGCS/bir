26817d7bc841c3fb2e39f221a63677c1fe930d5b
root <root@nf-test101.cl.cam.ac.uk>
2014-05-01 14:11:16 +0100

    FPGA Programming Scripts

---
+++ HW_stub/NetFPGA-10G-live/fpga_nadesh_prog_script.sh
@@ -0,0 +1,2 @@
+cd /root/netfpga/bir/HW_stub/NetFPGA-10G-live
+./tools/scripts/impact_run.sh projects/bir_starter/bitfiles/BIR_georgi_final.bit
---
+++ HW_stub/NetFPGA-10G-live/fpga_ref_prog_script.sh
@@ -0,0 +1,2 @@
+cd /root/netfpga/bir/HW_stub/NetFPGA-10G-live
+./tools/scripts/impact_run.sh /usr/local/P33/reference_router.bit 
